b0f5008b5b297880d096ebb8fe69e6b7ecc91195
RyanOrtiz0246 <ryan142310@gmail.com>
2024-10-05 09:59:12 -0600

    Homework_1

---
+++ src/Homework 1/Homework 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Declarar variables de diferentes tipos, asignarles valor e imprimir el valor.
+        int number = 23;
+        string text = "goodafternoon, i am Ryan";
+        double matricula = 2024.0246;
+        bool isTrue = true;
+
+        Console.WriteLine($"Int Number: {number}");
+        Console.WriteLine($"Text: {text}");
+        Console.WriteLine($"Decimal Number: {matricula}");
+        Console.WriteLine($"Is True: {isTrue}");
+
+        // 2. Declarar una constante en C# e imprimir el valor.
+        /*
+        En C#, las constantes se declaran utilizando la palabra clave const. Una constante
+        es un valor que no puede cambiar una vez que ha sido asignado.
+        */
+        const int eternal = 100;
+        Console.WriteLine($"Const: {eternal}");
+        // Intentar cambiar el valor de la constante (esto causará un error de compilación)
+
+        // 3. Declarar un entero, incrementarlo, decrementarlo, hacer operaciones con él.
+        int value = 20;
+        value++;
+        value--;
+        value += 10;
+        value *= 2;
+        Console.WriteLine($"Value after operations: {value}");
+
+        // 4. Declarar un float con valor 10152466.25. Declarar un byte que es igual a 5 + el float.
+        float bigNumber = 10152466.25f;
+        byte smallNumber = (byte)(5 + bigNumber);
+        Console.WriteLine("Float: " + bigNumber);
+        Console.WriteLine("Byte: " + smallNumber);// El valor puede ser inesperado debido al desbordamiento.
+
+        // 5. Imprimir la fecha y hora del sistema.
+        DateTime currentdate = DateTime.Now;
+        Console.WriteLine($"Current date and time: {currentdate}");
+    }
+}
